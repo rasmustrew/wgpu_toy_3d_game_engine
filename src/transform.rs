874b75721c36ecabb015381b9a924c5e5abe7132
Rasmus Thorsøe <rt071195@gmail.com@>
2021-12-19 14:44:28 +0100

    refactor to use world for storing and handling entities

--- src/transform.rs
+++ src/transform.rs
@@ -1,17 +1,16 @@
 use crate::model;
 
-pub struct Instance {
+pub struct Transform {
     pub position: cgmath::Vector3<f32>,
     pub rotation: cgmath::Quaternion<f32>,
 }
 
-impl Instance {
+impl Transform {
     pub fn to_raw(&self) -> Raw {
         let model =
             cgmath::Matrix4::from_translation(self.position) * cgmath::Matrix4::from(self.rotation);
         Raw {
             model: model.into(),
-            // NEW!
             normal: cgmath::Matrix3::from(self.rotation).into(),
         }
     }
@@ -30,20 +29,14 @@ impl model::Vertex for Raw {
         use std::mem;
         wgpu::VertexBufferLayout {
             array_stride: mem::size_of::<Raw>() as wgpu::BufferAddress,
-            // We need to switch from using a step mode of Vertex to Instance
-            // This means that our shaders will only change to use the next
-            // instance when the shader starts processing a new instance
             step_mode: wgpu::VertexStepMode::Instance,
             attributes: &[
+                //Position
                 wgpu::VertexAttribute {
                     offset: 0,
-                    // While our vertex shader only uses locations 0, and 1 now, in later tutorials we'll
-                    // be using 2, 3, and 4, for Vertex. We'll start at slot 5 not conflict with them later
                     shader_location: 5,
                     format: wgpu::VertexFormat::Float32x4,
                 },
-                // A mat4 takes up 4 vertex slots as it is technically 4 vec4s. We need to define a slot
-                // for each vec4. We don't have to do this in code though.
                 wgpu::VertexAttribute {
                     offset: mem::size_of::<[f32; 4]>() as wgpu::BufferAddress,
                     shader_location: 6,
@@ -59,7 +52,7 @@ impl model::Vertex for Raw {
                     shader_location: 8,
                     format: wgpu::VertexFormat::Float32x4,
                 },
-                // NEW!
+                //Rotation
                 wgpu::VertexAttribute {
                     offset: mem::size_of::<[f32; 16]>() as wgpu::BufferAddress,
                     shader_location: 9,
